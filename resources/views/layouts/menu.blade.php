0878b49effb5c64933e9f662fc69ab0030df5d08
AungZinTun <aungzintun2@gmail.com>
2019-06-17 14:49:47 +0630

    home'

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -289,10 +289,10 @@
       <!-- main menu footer-->
     </div>
     <!-- <li class="{{ Request::is('events*') ? 'active' : '' }}">
-    <a href="{!! route('events.index') !!}"><i class="fa fa-edit"></i><span>Events</span></a> -->
+    <a href="#"><i class="fa fa-edit"></i><span>Events</span></a> -->
 </li>
 
 <li class="{{ Request::is('roles*') ? 'active' : '' }}">
-    <a href="{!! route('roles.index') !!}"><i class="fa fa-edit"></i><span>Roles</span></a>
+    <a href="#"><i class="fa fa-edit"></i><span>Roles</span></a>
 </li>
 
